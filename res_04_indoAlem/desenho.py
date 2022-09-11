df4cec6ec64b07d315a9d748eedec376954c85a2
Pedro Augusto <pedro.augusto1@escolar.ifrn.edu.br>
2022-09-11 12:43:32 -0300

    Resolvendo exercicios

--- res_04_indoAlem/desenho.py
+++ res_04_indoAlem/desenho.py
@@ -1,6 +1,6 @@
 import turtle
 turtle = turtle.Turtle()
-"""
+
 for _ in range(360):
     turtle.forward(1)
     turtle.left(1)
@@ -22,34 +22,10 @@ turtle.circle(radius=200,steps=4)
 
 # Desafio 2
 for _ in range(3):
-    turtle.foward(100)
+    turtle.forward(100)
     turtle.right(120)
 
 for _ in range(4):
     turtle.forward(100)
     turtle.left(90)
-"""
-turtle.clear()
-# Desafio 3
 
-def reta(comprimento):
-    turtle.forward(comprimento)
-
-def triangulo(comprimento):
-    for _ in range(3):
-        turtle.forward(comprimento)
-        turtle.right(120)
-
-def quadrado(comprimento):
-    for _ in range(4):
-        turtle.forward(comprimento)
-        turlte.right(90)
-
-def estrela(comprimento):
-    for _ in range(3):
-        turtle.forward(comprimento)
-        turtle.left(90)
-        turtle.forward(comprimento)
-        turtle.left(140)
-
-estrela(300)
