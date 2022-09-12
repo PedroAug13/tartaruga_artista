5da08e7aa31cf40dc3877eb1614ad272ae406f09
Pedro Augusto <pedro.augusto1@escolar.ifrn.edu.br>
2022-09-12 11:48:51 -0300

    Organizando arquivos de resolucao de exercicios

--- res_03_HoraCodigo/hexagonoDeHexagonos.py
+++
@@ -1,9 +0,0 @@
-import turtle
-
-turtle = turtle.Turtle()
-
-for _ in range(6):
-    for _ in range(6):
-        turtle.forward(50)
-        turtle.right(60)
-    turtle.left(60)
--- res_03_HoraCodigo/hexagonoFor.py
+++
@@ -1,7 +0,0 @@
-import turtle
-
-turtle = turtle.Turtle()
-
-for _ in range(6):
-    turtle.forward(100)
-    turtle.right(60)
