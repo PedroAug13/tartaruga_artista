c99e416239e230bfd8ec7dd1d91eb8cb9a3e763f
Pedro Augusto <pedro.augusto1@escolar.ifrn.edu.br>
2022-09-13 18:59:28 -0300

    Exercicios da aula 12/09 concluidos

--- resolucoes_exercicios/Indo_Alem/formasGeometricasEventos.py
+++ resolucoes_exercicios/Indo_Alem/formasGeometricasEventos.py
@@ -2,6 +2,15 @@ import turtle
 from turtle import onscreenclick, mainloop, onkey, listen
 turtle = turtle.Turtle()
 
+def line(start,end):
+    turtle.penup()
+    turtle.goto(start[0],start[1])
+    turtle.pendown()
+    diff_x = abs(start[0] - end[0])
+    diff_y = abs(start[1] - end[1])
+    lenght = max(diff_x, diff_y)
+    turtle.forward(lenght)
+
 def triangle(start, end):
     turtle.penup()
     turtle.goto(start[0],start[1])
@@ -85,5 +94,6 @@ onkey(lambda: change_shape(square), "s")
 onkey(lambda: change_shape(circle), "c")
 onkey(lambda: change_shape(rectangle), "r")
 onkey(lambda: change_shape(triangle), "t")
+onkey(lambda: change_shape(line), "l")
 listen()
 mainloop()
