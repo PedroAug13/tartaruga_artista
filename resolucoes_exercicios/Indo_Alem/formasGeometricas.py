3941453372a2cbab1e347c189e67d1d78baf1a3d
Pedro Augusto <pedro.augusto1@escolar.ifrn.edu.br>
2022-09-13 18:46:01 -0300

    Separando formasGeometricas dos exercicios que foram feitos no dia 12/09

--- resolucoes_exercicios/Indo_Alem/formasGeometricas.py
+++ resolucoes_exercicios/Indo_Alem/formasGeometricas.py
@@ -1,89 +1,39 @@
 import turtle
-from turtle import onscreenclick, mainloop, onkey, listen
 turtle = turtle.Turtle()
 
-def triangle(start, end):
+def movimenta(comprimento):
+    turtle.setheading(0)
     turtle.penup()
-    turtle.goto(start[0],start[1])
-    turtle.pendown()   
-    diff_x = abs(start[0] - end[0])
-    diff_y = abs(start[1] - end[1])
-    width = max(diff_x,diff_y)
-    # Draws the figure
+    turtle.forward(comprimento)
+    turtle.pendown()
+
+def reta(comprimento):
+    turtle.forward(comprimento)
+
+def triangulo(comprimento):
     for _ in range(3):
-        turtle.forward(width)
+        turtle.forward(comprimento)
         turtle.right(120)
 
-def star(start, end):
-    turtle.penup()
-    turtle.goto(start[0],start[1])
-    turtle.pendown()   
-    diff_x = abs(start[0] - end[0])
-    diff_y = abs(start[1] - end[1])
-    point_size = max(diff_x,diff_y)
-    # Draws the figure
+def quadrado(comprimento):
+    for _ in range(4):
+        turtle.forward(comprimento)
+        turtle.left(90)
+
+def retangulo(comprimento):
+    for _ in range(2):
+        turtle.forward(comprimento / 2)
+        turtle.left(90)
+        turtle.forward(comprimento)
+        turtle.left(90)
+
+def estrela(comprimento):
     for _ in range(9):
-        turtle.forward(point_size)
+        turtle.forward(comprimento)
         turtle.left(130)
-        turtle.forward(point_size)
+        turtle.forward(comprimento)
         turtle.right(90)
 
-def circle(start,end):
-    turtle.penup()
-    turtle.goto(start[0],start[1])
-    turtle.pendown()   
-    diff_x = abs(start[0] - end[0])
-    diff_y = abs(start[1] - end[1])
-    radius = max(diff_x,diff_y)
-    # Draws the figure
-    turtle.circle(radius)
+def circulo(raio):
+    turtle.circle(raio)
 
-def square(start,end):
-    turtle.penup()
-    turtle.goto(start[0],start[1])
-    turtle.pendown()   
-    diff_x = abs(start[0] - end[0])
-    diff_y = abs(start[1] - end[1])
-    width = max(diff_x,diff_y)
-    # Draws the figure
-    for _ in range(4):
-        turtle.forward(width)
-        turtle.left(90)
-
-def rectangle(start, end):
-    turtle.penup()
-    turtle.goto(start[0],start[1])
-    turtle.pendown()   
-    diff_x = abs(start[0] - end[0])
-    diff_y = abs(start[1] - end[1])
-    width = max(diff_x,diff_y)
-    # Draws the figure
-    for _ in range(2):
-        turtle.forward(width)
-        turtle.left(90)
-        turtle.forward(width / 2)
-        turtle.left(90)
-
-start = None
-shape = square
-
-def tap(x,y):
-    global start
-    if start == None:
-        start = x,y
-    else:
-        end = x,y
-        shape(start,end)
-        start = None
-
-def change_shape(next_shape):
-    global shape
-    shape = next_shape
-
-onscreenclick(tap)
-onkey(lambda: change_shape(square), "s")
-onkey(lambda: change_shape(circle), "c")
-onkey(lambda: change_shape(rectangle), "r")
-onkey(lambda: change_shape(triangle), "t")
-listen()
-mainloop()
---
+++ resolucoes_exercicios/Indo_Alem/formasGeometricasEventos.py
@@ -0,0 +1,89 @@
+import turtle
+from turtle import onscreenclick, mainloop, onkey, listen
+turtle = turtle.Turtle()
+
+def triangle(start, end):
+    turtle.penup()
+    turtle.goto(start[0],start[1])
+    turtle.pendown()   
+    diff_x = abs(start[0] - end[0])
+    diff_y = abs(start[1] - end[1])
+    width = max(diff_x,diff_y)
+    # Draws the figure
+    for _ in range(3):
+        turtle.forward(width)
+        turtle.right(120)
+
+def star(start, end):
+    turtle.penup()
+    turtle.goto(start[0],start[1])
+    turtle.pendown()   
+    diff_x = abs(start[0] - end[0])
+    diff_y = abs(start[1] - end[1])
+    point_size = max(diff_x,diff_y)
+    # Draws the figure
+    for _ in range(9):
+        turtle.forward(point_size)
+        turtle.left(130)
+        turtle.forward(point_size)
+        turtle.right(90)
+
+def circle(start,end):
+    turtle.penup()
+    turtle.goto(start[0],start[1])
+    turtle.pendown()   
+    diff_x = abs(start[0] - end[0])
+    diff_y = abs(start[1] - end[1])
+    radius = max(diff_x,diff_y)
+    # Draws the figure
+    turtle.circle(radius)
+
+def square(start,end):
+    turtle.penup()
+    turtle.goto(start[0],start[1])
+    turtle.pendown()   
+    diff_x = abs(start[0] - end[0])
+    diff_y = abs(start[1] - end[1])
+    width = max(diff_x,diff_y)
+    # Draws the figure
+    for _ in range(4):
+        turtle.forward(width)
+        turtle.left(90)
+
+def rectangle(start, end):
+    turtle.penup()
+    turtle.goto(start[0],start[1])
+    turtle.pendown()   
+    diff_x = abs(start[0] - end[0])
+    diff_y = abs(start[1] - end[1])
+    width = max(diff_x,diff_y)
+    # Draws the figure
+    for _ in range(2):
+        turtle.forward(width)
+        turtle.left(90)
+        turtle.forward(width / 2)
+        turtle.left(90)
+
+start = None
+shape = square
+
+def tap(x,y):
+    global start
+    if start == None:
+        start = x,y
+    else:
+        end = x,y
+        shape(start,end)
+        start = None
+
+def change_shape(next_shape):
+    global shape
+    shape = next_shape
+
+onscreenclick(tap)
+onkey(lambda: change_shape(square), "s")
+onkey(lambda: change_shape(circle), "c")
+onkey(lambda: change_shape(rectangle), "r")
+onkey(lambda: change_shape(triangle), "t")
+listen()
+mainloop()
