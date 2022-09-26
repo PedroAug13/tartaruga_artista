6a840c80f433f8438bc98bd29e09bef3db8f6d54
Pedro Augusto <pedro.augusto1@escolar.ifrn.edu.br>
2022-09-25 22:01:52 -0300

    add toggle fill

--- resolucoes_exercicios/freepygames/paint.py
+++ resolucoes_exercicios/freepygames/paint.py
@@ -1,13 +1,11 @@
 """Paint, for drawing shapes.
-
-Exercises
-
+Exercises ~~~ Done
 . Add a color.
 . Complete circle.
 . Complete rectangle.
 . Complete triangle.
 . Add width parameter.
-6. Toggle fill/no fill
+. Toggle fill/no fill 
 . Remove repeated code in figure functions
 template method -> design patterns for OO
 
@@ -16,6 +14,7 @@ template method -> design patterns for OO
 from turtle import setup, onscreenclick, listen, onkey, undo, done, up, goto, down, mainloop, begin_fill, end_fill, forward, left, right, circle, color, pensize
 from freegames import vector
 
+
 def goto_position(start, end):
     up()
     goto(start.x, start.y)
@@ -23,65 +22,39 @@ def goto_position(start, end):
 
 def line(start, end):
     """Draw line from start to end."""
-    goto_position(start,end)
     goto(end.x, end.y)
-
 
 def square(start, end):
     """Draw square from start to end."""
-    goto_position(start,end)
-    begin_fill()
     for count in range(4):
         forward(end.x - start.x)
         left(90)
-    end_fill()
-
 
 def circles(start, end):
     """Draw circle from start to end."""
-    goto_position(start,end)
-    begin_fill()
     circle(end.x - start.x)
-    end_fill()
-
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    goto_position(start,end)
-    begin_fill()
     lenght = end.x - start.x
     for count in range(2):
         forward(lenght)
         left(90)
         forward(lenght / 2)
         left(90)
-    end_fill()
-
 
 def triangle(start, end):
     """Draw triangle from start to end."""
-    goto_position(start,end)
-    begin_fill()
     for count in range(3):
         forward(end.x - start.x)
         left(120)
 
-    end_fill()
-
-
-def tap(x, y):
-    """Store starting point or draw shape."""
-    start = state['start']
-
-    if start is None:
-        # state['start'] = vector(x, y)
-        store('start', vector(x, y))
+def toggle_fill():
+    fill_state = state['fill'] 
+    if fill_state == True:
+        state['fill'] = False
     else:
-        shape = state['shape']
-        end = vector(x, y)
-        shape(start, end)
-        # state['start'] = None
-        store('start', None)
+        state['fill'] = True
 
 def store(key, value):
     """Store value in state at key."""
@@ -98,12 +71,33 @@ def decrease_pensize():
         pensize(size)
         state['size'] = size
 
-state = {'start': None, 'shape': line, 'size': 1, 'fill': }
+def draw_shape(x, y):
+    """Store starting point or draw shape."""
+    start = state['start']
+
+    if start is None:
+        # state['start'] = vector(x, y)
+        store('start', vector(x, y))
+    else:
+        shape = state['shape']
+        end = vector(x, y)
+        goto_position(start,end)
+        if state['fill'] == True:
+            begin_fill()
+            shape(start, end)
+            end_fill()
+        else:
+            shape(start, end)
+        # state['start'] = None
+        store('start', None)
+
+state = {'start': None, 'shape': line, 'size': 1, 'fill': True}
 
 setup(1600, 900)
-onscreenclick(tap)
+onscreenclick(draw_shape)
 listen()
 onkey(undo, 'u')
+onkey(toggle_fill, 'f')
 onkey(increase_pensize, 'Up')
 onkey(decrease_pensize, 'Down')
 onkey(lambda: color('purple'), 'P')
